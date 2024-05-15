bf3bb3e52539de3a682a60caaa6ddc1e0d2badf0
rizkydanur <rizkydanur1@gmail.com>
2024-05-15 09:46:58 +0700

    update database
    menambahkan parkir masuk dan keluar

--- app/DATABASE_MYSQL/adminParkir.sql
+++ app/DATABASE_MYSQL/adminParkir.sql
@@ -1,71 +1,158 @@
--- Adminer 4.8.1 MySQL 8.0.30 dump
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:8889
+-- Waktu pembuatan: 15 Bulan Mei 2024 pada 02.45
+-- Versi server: 5.7.39
+-- Versi PHP: 7.4.33
 
-SET NAMES utf8;
-SET time_zone = '+00:00';
-SET foreign_key_checks = 0;
-SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-SET NAMES utf8mb4;
 
-DROP TABLE IF EXISTS `failed_jobs`;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `adminParkir`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `failed_jobs`
+--
+
 CREATE TABLE `failed_jobs` (
-  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `id` bigint(20) UNSIGNED NOT NULL,
   `uuid` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `connection` text COLLATE utf8mb4_unicode_ci NOT NULL,
   `queue` text COLLATE utf8mb4_unicode_ci NOT NULL,
   `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
   `exception` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
-  `failed_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `failed_jobs_uuid_unique` (`uuid`)
+  `failed_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+-- --------------------------------------------------------
 
-DROP TABLE IF EXISTS `migrations`;
+--
+-- Struktur dari tabel `migrations`
+--
+
 CREATE TABLE `migrations` (
-  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `id` int(10) UNSIGNED NOT NULL,
   `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `batch` int NOT NULL,
-  PRIMARY KEY (`id`)
+  `batch` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data untuk tabel `migrations`
+--
 
 INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
-(1,	'2014_10_12_000000_create_users_table',	1),
-(2,	'2014_10_12_100000_create_password_reset_tokens_table',	1),
-(3,	'2019_08_19_000000_create_failed_jobs_table',	1),
-(4,	'2019_12_14_000001_create_personal_access_tokens_table',	1),
-(5,	'2024_05_11_065335_add_type_to_users_table',	1);
+(1, '2014_10_12_000000_create_users_table', 1),
+(2, '2014_10_12_100000_create_password_reset_tokens_table', 1),
+(3, '2019_08_19_000000_create_failed_jobs_table', 1),
+(4, '2019_12_14_000001_create_personal_access_tokens_table', 1),
+(5, '2024_05_11_065335_add_type_to_users_table', 1),
+(6, '2014_10_12_100000_create_password_resets_table', 2),
+(7, '2024_05_14_100405_create_parkir_masuk_table', 2),
+(8, '2024_05_14_100450_create_parkir_keluar_table', 2),
+(9, '2024_05_15_015415_create_parkir_masuks_table', 3),
+(10, '2024_05_15_015746_create_parkir_keluars_table', 4);
 
-DROP TABLE IF EXISTS `password_reset_tokens`;
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `parkir_keluars`
+--
+
+CREATE TABLE `parkir_keluars` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `no_polisi` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `id_kartu` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `jam_masuk` datetime NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `parkir_masuks`
+--
+
+CREATE TABLE `parkir_masuks` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `no_polisi` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `id_kartu` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `jam_masuk` datetime NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data untuk tabel `parkir_masuks`
+--
+
+INSERT INTO `parkir_masuks` (`id`, `no_polisi`, `id_kartu`, `jam_masuk`, `created_at`, `updated_at`) VALUES
+(1, 'B 123 CDE', '123456', '2024-05-15 02:19:49', NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `password_resets`
+--
+
+CREATE TABLE `password_resets` (
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `password_reset_tokens`
+--
+
 CREATE TABLE `password_reset_tokens` (
   `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`email`)
+  `created_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+-- --------------------------------------------------------
 
-DROP TABLE IF EXISTS `personal_access_tokens`;
+--
+-- Struktur dari tabel `personal_access_tokens`
+--
+
 CREATE TABLE `personal_access_tokens` (
-  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `id` bigint(20) UNSIGNED NOT NULL,
   `tokenable_type` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `tokenable_id` bigint unsigned NOT NULL,
+  `tokenable_id` bigint(20) UNSIGNED NOT NULL,
   `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `token` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
   `abilities` text COLLATE utf8mb4_unicode_ci,
   `last_used_at` timestamp NULL DEFAULT NULL,
   `expires_at` timestamp NULL DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `personal_access_tokens_token_unique` (`token`),
-  KEY `personal_access_tokens_tokenable_type_tokenable_id_index` (`tokenable_type`,`tokenable_id`)
+  `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+-- --------------------------------------------------------
 
-DROP TABLE IF EXISTS `users`;
+--
+-- Struktur dari tabel `users`
+--
+
 CREATE TABLE `users` (
-  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `id` bigint(20) UNSIGNED NOT NULL,
   `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `email_verified_at` timestamp NULL DEFAULT NULL,
@@ -73,13 +160,117 @@ CREATE TABLE `users` (
   `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
-  `type` tinyint NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `users_email_unique` (`email`)
+  `type` tinyint(4) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`, `type`) VALUES
-(1,	'Admin User',	'admin@parkir.com',	NULL,	'$2y$12$sDRR7.Rvt7edB3/CSn86p.JhC7/r6mhDXJQoEe2lgMnWgbHTwzQ2e',	NULL,	'2024-05-11 00:13:24',	'2024-05-11 00:13:24',	1),
-(2,	'User',	'user@parkir.com',	NULL,	'$2y$12$hw8AG9YRybj.BtLC6voCfOoQO3fI9Nx9OjH2xpPCvmLMfx.zqaPHO',	NULL,	'2024-05-11 00:13:24',	'2024-05-11 00:13:24',	0);
+--
+-- Dumping data untuk tabel `users`
+--
 
--- 2024-05-11 09:07:45
+INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`, `type`) VALUES
+(9, 'Admin User', 'admin@parkir.com', NULL, '$2y$12$r0AIgJGWVyMkEsu/8.xEJ.9f1xs9nYwAf7BMMiUOuMMnWkMcHzMYu', NULL, '2024-05-13 23:07:30', '2024-05-13 23:23:44', 1),
+(10, 'User', 'user@parkir.com', NULL, '$2y$12$gWIHpfh9fbLPl/MI6A1y6uNMBwK/zsvbmeY/Uqdr1T1LGGnkOvVnG', NULL, '2024-05-13 23:07:30', '2024-05-14 01:41:18', 0),
+(11, 'user2', 'user2@parkir.com', NULL, '$2y$12$MzhtJs7QAuCUfEhM.OSnYuRjXnDMfCCEFleiLGyKiexwUOpy2XegG', NULL, '2024-05-13 23:15:57', '2024-05-14 02:44:36', 0),
+(12, 'user3', 'user3@parkir.com', NULL, '$2y$12$6poHJkiWNLyNHl0GamSy4eKzudYEFUOVPJXpfWSt7VdPW1yoP4.sy', NULL, '2024-05-13 23:31:20', '2024-05-14 02:46:07', 0),
+(16, 'admin1', 'admin1@gmail.com', NULL, '$2y$12$oRkutn2qAzDGv7FgPtzN4eauxP28UQVC.NK6jgVxd1UpY3PMU4riG', NULL, '2024-05-14 02:15:44', '2024-05-14 02:37:30', 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `failed_jobs_uuid_unique` (`uuid`);
+
+--
+-- Indeks untuk tabel `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `parkir_keluars`
+--
+ALTER TABLE `parkir_keluars`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `parkir_masuks`
+--
+ALTER TABLE `parkir_masuks`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `password_resets`
+--
+ALTER TABLE `password_resets`
+  ADD KEY `password_resets_email_index` (`email`);
+
+--
+-- Indeks untuk tabel `password_reset_tokens`
+--
+ALTER TABLE `password_reset_tokens`
+  ADD PRIMARY KEY (`email`);
+
+--
+-- Indeks untuk tabel `personal_access_tokens`
+--
+ALTER TABLE `personal_access_tokens`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `personal_access_tokens_token_unique` (`token`),
+  ADD KEY `personal_access_tokens_tokenable_type_tokenable_id_index` (`tokenable_type`,`tokenable_id`);
+
+--
+-- Indeks untuk tabel `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `users_email_unique` (`email`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT untuk tabel `parkir_keluars`
+--
+ALTER TABLE `parkir_keluars`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `parkir_masuks`
+--
+ALTER TABLE `parkir_masuks`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT untuk tabel `personal_access_tokens`
+--
+ALTER TABLE `personal_access_tokens`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
