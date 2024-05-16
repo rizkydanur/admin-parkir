612a9b265deda319ef23179c3ef2b04f224e7f8d
rizkydanur <rizkydanur1@gmail.com>
2024-05-16 17:31:14 +0700

    zz
    zz

--- resources/views/admin/layouts/layout.blade.php
+++ resources/views/admin/layouts/layout.blade.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Elegant Dashboard | Dashboard</title>
+  <title>Admin | Dashboard</title>
   <!-- Favicon -->
   <link rel="shortcut icon" href="{{ asset('assets/img/svg/logo.svg') }}" type="image/x-icon">
    <!-- Custom styles -->
